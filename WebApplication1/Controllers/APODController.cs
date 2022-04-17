bdcfb91c8a0e1386bcd0ecf6e7eb931fc2f9dd30
Ivan-Ivanov-SE-2001321013 <stu2001321013@uni-plovdiv.bg>
2022-04-17 16:14:29 +0300

    Add project files.

---
+++ WebApplication1/Controllers/APODController.cs
@@ -0,0 +1,69 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class APODController : Controller
+    {
+        // GET: APOD
+        //----> NAI - GOLEMITEEEEE!!! <----
+        public ActionResult Index()
+        {
+            ViewData["APOD"] = "Hello world!!!";
+            var api_key = "SY9gLxnPM6sUEjGfEe1OYpbcQxJMUBeVYhwkn085";
+
+            var url = "https://api.nasa.gov/planetary/apod?api_key=" + api_key;
+
+
+            var client = new WebClient();
+
+            var body = "empty";
+
+            body = client.DownloadString(url);
+
+            JObject data = JObject.Parse(body);
+
+
+            ViewData["Date"] = DateTime.Now.Date;
+            ViewData["explanation"] = data["explanation"];
+
+            ViewData["hdurl"] = data["hdurl"];
+
+            ViewData["body"] = body;
+
+            return View();
+        }
+
+        public ActionResult HomeWork()
+        {
+
+            var name = "ivan";
+
+            var url = "https://api.genderize.io/?name=" + name;
+
+
+            var client = new WebClient();
+
+            var body = "empty";
+
+            body = client.DownloadString(url);
+
+            JObject data = JObject.Parse(body);
+
+
+            ViewData["name"] = data["name"];
+
+            ViewData["gender"] = data["gender"];
+            ViewData["probability"] = data["probability"];
+
+            ViewData["count"] = data["count"];
+
+            return View();
+        }
+    }
+}
---
+++ WebApplication1/Controllers/ExampleController.cs
@@ -0,0 +1,48 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class ExampleController : Controller
+    {
+        // GET: Example
+        public ActionResult BGToll(string number)
+        {
+            ViewData["search"] = number;
+
+            var url = "https://check.bgtoll.bg/check/vignette/plate/BG/" + number;
+
+            var client = new WebClient();
+
+            var body = "empty";
+
+            if (number != null && number != "")
+            {
+                body = client.DownloadString(url);
+
+                JObject data = JObject.Parse(body);
+
+                if ((bool)data["ok"] == true)
+                {
+                    ViewData["result"] = "Намерена е винетка за номер " + number;
+
+                    ViewData["vNumber"] = data["vignette"]["vignetteNumber"];
+                    ViewData["active"] = data["vignette"]["validityDateToFormated"];
+                    ViewData["class"] = data["vignette"]["vehicleClass"];
+                }
+                else
+                {
+                    ViewData["result"] = number + " няма активна винетка.";
+                }
+            }
+            ViewData["body"] = body;
+
+            return View();
+        }
+    }
+}
